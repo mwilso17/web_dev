e14b8519bf2c6a4f6551117241ce7ad68001afa0
mwilso17 <michael.b.wilson2@gmail.com>
2025-05-06 20:29:46 -0400

    javascript template added, small fixes to css links, update css for templates.

---
+++ templates/sample.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var button = document.getElementById("clickIt");
+  button.onclick = function() {
+      alert("Button Clicked!");
+  };
+});
