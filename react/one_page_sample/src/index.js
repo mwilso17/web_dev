c3466c6b85c9604ad953af526ff4d83ba81ef051
mwilso17 <michael.b.wilson2@gmail.com>
2025-06-16 18:13:04 -0400

    Set up dependencies for local dev environment.

--- react/one_page_sample/src/index.js
+++ react/one_page_sample/src/index.js
@@ -1,1 +1,11 @@
-console.log('test test test')
+import ReactDOM from 'react-dom/client';
+
+ function App() {
+  return <h1>This is a test.</h1>;
+ }
+
+ const container = document.getElementById('app');
+
+ const root = ReactDOM.createRoot(container);
+
+ root.render(<App />);
